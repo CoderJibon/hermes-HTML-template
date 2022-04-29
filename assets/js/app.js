c15831146e44392a504a2822e0540c319daefde3
coderjibon <contact.engjibon@gmail.com>
2022-04-29 17:37:14 +0600

    slider section done

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function () {
+    
+      $('.sliderbox').slick({
+        centerMode: false,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: `<div id="coderprev" class="slick-prev">
+                    <img src="./assets/img/Arrow 2.png" alt="" />
+                </div> `,
+        nextArrow: `<div id="codernext" class="slick-prev">
+                    <img src="./assets/img/Arrow 3.png" alt="" />
+                    </div>`,
+        pauseOnHover:false,
+        responsive: [
+            {
+            breakpoint: 768,
+            settings: {
+                centerPadding: '40px',
+                slidesToShow: 3
+                }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                centerPadding: '40px',
+                slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
