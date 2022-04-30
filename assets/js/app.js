29fcbe5e2fcf6031932e339689966ff80d6cdfb5
coderjibon <contact.engjibon@gmail.com>
2022-04-30 18:25:08 +0600

    text update

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
     
     $('.slider').slick({
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2000,
         fade: true,
         arrows: false,
          pauseOnHover:false,
