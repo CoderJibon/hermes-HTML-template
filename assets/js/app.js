f9ef4edea81dbdba32809fdb512aef88ba32a4a9
coderjibon <contact.engjibon@gmail.com>
2022-04-30 16:26:37 +0600

    home slider up

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,5 +30,15 @@ $(document).ready(function () {
                 }
             }
         ]
+      });
+    
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        arrows: false,
+         pauseOnHover:false,
     });
+    
+    
 });
